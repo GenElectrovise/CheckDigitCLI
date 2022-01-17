6f939e02b306f34b6252090c625045f0319f5a61
Adam Spencer <46679475+GenElectrovise@users.noreply.github.com>
2022-01-17 19:50:27 +0000

    Switched IDE to IntelliJ for the lols

--- src/main/java/checkdigitcli/command/GenerateCheckDigits.java
+++ src/main/java/checkdigitcli/command/GenerateCheckDigits.java
@@ -18,163 +18,163 @@ import checkdigitcli.main.CheckDigitCLIException;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-@Command(name = "generateCheckDigits", aliases = { "gcd" })
+@Command(name = "generateCheckDigits", aliases = {"gcd"})
 public class GenerateCheckDigits implements Runnable {
 
-	@Option(names = { "-if", "-inputFile" }, required = false)
-	private File inputFile;
+    @Option(names = {"-if", "-inputFile"}, required = false)
+    private File inputFile;
 
-	@Option(names = { "-p", "-payloads" }, required = false)
-	private String[] manualPayloads;
+    @Option(names = {"-p", "-payloads"}, required = false)
+    private String[] manualPayloads;
 
-	@Option(names = { "-of", "-outputFile" }, required = false)
-	private File outputFile;
+    @Option(names = {"-of", "-outputFile"}, required = false)
+    private File outputFile;
 
-	@Option(names = { "-a", "-algorithm" }, required = true)
-	private String algorithmName;
+    @Option(names = {"-a", "-algorithm"}, required = true)
+    private String algorithmName;
 
-	@Option(names = { "-k", "-kernel", "-gpu" }, required = false, defaultValue = "true")
-	private boolean useKernel;
+    @Option(names = {"-k", "-kernel", "-gpu"}, required = false, defaultValue = "true")
+    private boolean useKernel;
 
-	@Override
-	public void run() {
-		try {
-			Algorithm algorithm = Algorithms.getAlgorithm(algorithmName);
-			Stream<String> payloads = collectPayloads(inputFile, manualPayloads);
-			byte[][] digits = applyAlgorithm(algorithm, payloads);
-			displayResults(digits, outputFile);
+    @Override
+    public void run() {
+        try {
+            Algorithm algorithm = Algorithms.getAlgorithm(algorithmName);
+            Stream<String> payloads = collectPayloads(inputFile, manualPayloads);
+            byte[][] digits = applyAlgorithm(algorithm, payloads);
+            displayResults(digits, outputFile);
 
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		} catch (CheckDigitCLIException e) {
-			System.err.println(e.getMessage());
-			return;
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        } catch (CheckDigitCLIException e) {
+            System.err.println(e.getMessage());
+            return;
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	private void displayResults(byte[][] digits, File outputFile) throws IOException, InterruptedException {
+    private void displayResults(byte[][] digits, File outputFile) throws IOException, InterruptedException {
 
-		String[] results = formatResults(digits);
+        String[] results = formatResults(digits);
 
-		if (outputFile != null) {
-			FileUtils.printOneLineResultsToOutputFile(results, outputFile);
-		} else {
-			FileUtils.printOneLineResultsToConsole(results);
-		}
+        if (outputFile != null) {
+            FileUtils.printOneLineResultsToOutputFile(results, outputFile);
+        } else {
+            FileUtils.printOneLineResultsToConsole(results);
+        }
 
-	}
+    }
 
-	private String[] formatResults(byte[][] digits) {
+    private String[] formatResults(byte[][] digits) {
 
-		String[] out = new String[digits.length];
+        String[] out = new String[digits.length];
 
-		for (int i = 0; i < digits.length; i++) {
+        for (int i = 0; i < digits.length; i++) {
 
-			StringBuilder builder = new StringBuilder();
+            StringBuilder builder = new StringBuilder();
 
-			for (int j = 0; j < digits[i].length; j++) {
-				builder.append(digits[i][j]);
-			}
+            for (int j = 0; j < digits[i].length; j++) {
+                builder.append(digits[i][j]);
+            }
 
-			out[i] = builder.toString();
-		}
+            out[i] = builder.toString();
+        }
 
-		return out;
-	}
+        return out;
+    }
 
-	private byte[][] applyAlgorithm(Algorithm algorithm, Stream<String> sPayloads) {
+    private byte[][] applyAlgorithm(Algorithm algorithm, Stream<String> sPayloads) {
 
-		long start = System.nanoTime();
+        long start = System.nanoTime();
 
-		List<String> lPayloads = sPayloads.collect(Collectors.toList());
+        List<String> lPayloads = sPayloads.collect(Collectors.toList());
 
-		// Generate preview
-		final int numberToPreview = 10;
-		String payloadsPreview = "";
-		for (int i = 0; i < (lPayloads.size() < numberToPreview ? lPayloads.size() : numberToPreview); i++) {
-			payloadsPreview += (payloadsPreview.equals("") ? "" : " , ") + lPayloads.get(i);
-		}
-		if (lPayloads.size() > numberToPreview)
-			payloadsPreview += " ... (" + (lPayloads.size() - numberToPreview) + " more)";
+        // Generate preview
+        final int numberToPreview = 10;
+        String payloadsPreview = "";
+        for (int i = 0; i < (lPayloads.size() < numberToPreview ? lPayloads.size() : numberToPreview); i++) {
+            payloadsPreview += (payloadsPreview.equals("") ? "" : " , ") + lPayloads.get(i);
+        }
+        if (lPayloads.size() > numberToPreview)
+            payloadsPreview += " ... (" + (lPayloads.size() - numberToPreview) + " more)";
 
-		// Show preview
-		System.out.println("Using " + algorithm.getName() + " algorithm");
-		System.out.println("Found " + lPayloads.size() + " payloads: " + payloadsPreview);
+        // Show preview
+        System.out.println("Using " + algorithm.getName() + " algorithm");
+        System.out.println("Found " + lPayloads.size() + " payloads: " + payloadsPreview);
 
-		// Apply algorithm to all
-		// Create input/output arrays
-		byte[][] inputs = new byte[lPayloads.size()][]; // Height=long, Width=2
-		byte[][] outputs = inputs.clone();
-		
-		/*
-		 * [i] = [ pay load , CD ]
-		 * [0] = [6, 6, 3, 4, CD ]
-		 * [1] = [2, 3, 1, 7, CD ]
-		 */
+        // Apply algorithm to all
+        // Create input/output arrays
+        byte[][] inputs = new byte[lPayloads.size()][]; // Height=long, Width=2
+        byte[][] outputs = inputs.clone();
 
-		// Fill input array
-		for (int i = 0; i < lPayloads.size(); i++) {
-			inputs[i] = lPayloads.get(i).getBytes();
-		}
+        /*
+         * [i] = [ pay load , CD ]
+         * [0] = [6, 6, 3, 4, CD ]
+         * [1] = [2, 3, 1, 7, CD ]
+         */
 
-		System.out.println(useKernel ? "Using Aparapi kernel" : "Not using Aparapi kernel");
-		// To use the kernel, or not to use the kernel, that is the if-statement: 
-		if (useKernel) {
+        // Fill input array
+        for (int i = 0; i < lPayloads.size(); i++) {
+            inputs[i] = lPayloads.get(i).getBytes();
+        }
 
-			LuhnKernel kernel = new LuhnKernel(inputs, outputs);
-			kernel.execute(inputs.length);
-			
-			// kernel.get(kernel.getOutputs()); // Fetch the buffer from the GPU
-			outputs = kernel.getOutputs(); 
-			
-		} else /* (!useKernel) */ {
-			
-			for (int i = 0; i < inputs.length; i++) {
-				int digit = algorithm.generate(inputs[i]);
-				
-				outputs[i] = new byte[inputs[i].length + 1];
-				outputs[i][outputs[i].length - 1] = (byte) digit;
-				outputs[i][1] = (byte) 0xFF; //String.valueOf(digit).getBytes();
-				
-				/*
-				 * [i] = [ pay load , CD ]
-				 * [0] = [6, 6, 3, 4, CD ]
-				 * [1] = [2, 3, 1, 7, CD ]
-				 */
-			}
-			
-		}
+        System.out.println(useKernel ? "Using Aparapi kernel" : "Not using Aparapi kernel");
+        // To use the kernel, or not to use the kernel, that is the if-statement:
+        if (useKernel) {
 
-		/*
-		 * for (int i = 0; i < lPayloads.size(); i++) { String p = lPayloads.get(i); int
-		 * digit = algorithm.generate(p.getBytes()); outputs[i][0] = p; outputs[i][1] =
-		 * String.valueOf(digit); }
-		 */
+            LuhnKernel kernel = new LuhnKernel(inputs, outputs);
+            kernel.execute(inputs.length);
 
-		System.out.println(String.format("Took %dms to compute %d values", ((System.nanoTime() - start) / 1_000_000), lPayloads.size()));
+            // kernel.get(kernel.getOutputs()); // Fetch the buffer from the GPU
+            outputs = kernel.getOutputs();
 
-		return outputs;
-	}
+        } else /* (!useKernel) */ {
 
-	private Stream<String> collectPayloads(File inputFile, String[] manualPayloads) throws IOException, CheckDigitCLIException {
+            for (int i = 0; i < inputs.length; i++) {
+                int digit = algorithm.generate(inputs[i]);
 
-		Stream<String> filePayloads = FileUtils.getStreamOfLines(inputFile);
-		Stream<String> sManualPayloads = (manualPayloads != null ? Stream.of(manualPayloads) : Stream.empty());
+                outputs[i] = new byte[inputs[i].length + 1];
+                outputs[i][outputs[i].length - 1] = (byte) digit;
+                outputs[i][1] = (byte) 0xFF; //String.valueOf(digit).getBytes();
 
-		return Stream.concat(filePayloads, sManualPayloads);
-	}
+                /*
+                 * [i] = [ pay load , CD ]
+                 * [0] = [6, 6, 3, 4, CD ]
+                 * [1] = [2, 3, 1, 7, CD ]
+                 */
+            }
 
-	public static class ResultCompressorKernel extends Kernel {
+        }
 
-		@Override
-		public void run() {
+        /*
+         * for (int i = 0; i < lPayloads.size(); i++) { String p = lPayloads.get(i); int
+         * digit = algorithm.generate(p.getBytes()); outputs[i][0] = p; outputs[i][1] =
+         * String.valueOf(digit); }
+         */
 
-		}
+        System.out.println(String.format("Took %dms to compute %d values", ((System.nanoTime() - start) / 1_000_000), lPayloads.size()));
 
-	}
+        return outputs;
+    }
+
+    private Stream<String> collectPayloads(File inputFile, String[] manualPayloads) throws IOException, CheckDigitCLIException {
+
+        Stream<String> filePayloads = FileUtils.getStreamOfLines(inputFile);
+        Stream<String> sManualPayloads = (manualPayloads != null ? Stream.of(manualPayloads) : Stream.empty());
+
+        return Stream.concat(filePayloads, sManualPayloads);
+    }
+
+    public static class ResultCompressorKernel extends Kernel {
+
+        @Override
+        public void run() {
+
+        }
+
+    }
 
 }
--- src/main/java/checkdigitcli/main/CheckDigitCLI.java
+++ src/main/java/checkdigitcli/main/CheckDigitCLI.java
@@ -10,73 +10,73 @@ import picocli.CommandLine;
 
 public class CheckDigitCLI {
 
-	private static boolean terminating = false;
+    private static boolean terminating = false;
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 
-		System.out.println("Checking for a console...");
+        System.out.println("Checking for a console...");
 
-		if (System.console() != null) {
-			System.out.println("Running in a console environment!");
-			runOnConsole();
-		} else {
-			try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File("CheckDigitCLI_ERROR.txt")))) {
-				writer.write("CheckDigitCLI must be run with access to a console.\n");
-				writer.write("https://github.com/GenElectrovise/CheckDigitCLI");
-			}
-		}
-	}
+        if (System.console() != null) {
+            System.out.println("Running in a console environment!");
+            runOnConsole();
+        } else {
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File("CheckDigitCLI_ERROR.txt")))) {
+                writer.write("CheckDigitCLI must be run with access to a console.\n");
+                writer.write("https://github.com/GenElectrovise/CheckDigitCLI");
+            }
+        }
+    }
 
-	private static void runOnConsole() {
-		System.out.println("Starting interactive CLI...");
-		clearConsole();
-		System.out.println("== CheckDigitCLI ==");
-		System.out.println("Copyright (c) GenElectrovise 2022");
-		System.out.println("https://github.com/GenElectrovise/CheckDigitCLI");
-		System.out.println(" ");
-		System.out.println("Use command 'help' for help");
+    private static void runOnConsole() {
+        System.out.println("Starting interactive CLI...");
+        clearConsole();
+        System.out.println("== CheckDigitCLI ==");
+        System.out.println("Copyright (c) GenElectrovise 2022");
+        System.out.println("https://github.com/GenElectrovise/CheckDigitCLI");
+        System.out.println(" ");
+        System.out.println("Use command 'help' for help");
 
-		while (!terminating) {
-			pollCommand();
-		}
+        while (!terminating) {
+            pollCommand();
+        }
 
-		System.out.println("Bye bye :'(");
-		System.out.println("Exiting normally... (0)");
-		System.exit(0);
-	}
+        System.out.println("Bye bye :'(");
+        System.out.println("Exiting normally... (0)");
+        System.exit(0);
+    }
 
-	private static void pollCommand() {
-		System.out.println("\n");
-		String[] command = System.console().readLine().split(" ");
+    private static void pollCommand() {
+        System.out.println("\n");
+        String[] command = System.console().readLine().split(" ");
 
-		if (command[0].equals("cdcli")) {
-			String[] tmp = new String[command.length - 1];
-			System.arraycopy(command, 1, tmp, 0, command.length - 1);
-			command = tmp;
-		}
+        if (command[0].equals("cdcli")) {
+            String[] tmp = new String[command.length - 1];
+            System.arraycopy(command, 1, tmp, 0, command.length - 1);
+            command = tmp;
+        }
 
-		/*
-		 * // Debug, pring args for (int i = 0; i < command.length; i++) {
-		 * System.out.println("    - " + command[i]); }
-		 */
+        /*
+         * // Debug, print args for (int i = 0; i < command.length; i++) {
+         * System.out.println("    - " + command[i]); }
+         */
 
-		new CommandLine(new CDCLI()).execute(command);
-	}
+        new CommandLine(new CDCLI()).execute(command);
+    }
 
-	public static void clearConsole() {
-		try {
-			new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-		} catch (InterruptedException | IOException e) {
-			System.out.println("Unable to clear console!");
-			e.printStackTrace();
-		}
-	}
+    public static void clearConsole() {
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch (InterruptedException | IOException e) {
+            System.out.println("Unable to clear console!");
+            e.printStackTrace();
+        }
+    }
 
-	public static boolean isTerminating() {
-		return terminating;
-	}
+    public static boolean isTerminating() {
+        return terminating;
+    }
 
-	public static void setTerminating(boolean terminating) {
-		CheckDigitCLI.terminating = terminating;
-	}
+    public static void setTerminating(boolean terminating) {
+        CheckDigitCLI.terminating = terminating;
+    }
 }
